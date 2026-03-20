bf1289c33a69e2873f9f77f5c2ad54a3aadbab3d
ceruleansk1es <134158860+ceruleansk1es@users.noreply.github.com>
2026-03-20 15:03:50 -0500

    Add files via upload

---
+++ PriorityQueue.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <functional>
+#include <utility>
+#include <vector>
+
+template <class T, class Container = std::vector<T>, class Compare = std::less<T>>
+class PriorityQueue {
+public:
+    using value_compare = Compare;
+    using value_type = T;
+    using size_type = size_t;
+    using reference = T&;
+    using const_reference = const T&;
+
+private:
+    Container c;
+    value_compare comp;
+
+    size_type parent(size_type index) { return (index - 1) / 2; }
+    size_type left_child(size_type index) { return 2 * index + 1; }
+    size_type right_child(size_type index) { return 2 * (index + 1); }
+    bool is_internal(size_t index) { return left_child(index) < c.size(); }
+    bool is_leaf(size_t index) { return left_child(index) >= c.size(); }
+
+    
+    /**
+     * @brief Recursively (or Iteratively) swap the value at index up the heap until it is in the correct position.
+     * 
+     * @note This is a max heap, so assume Compare is less, but promote the larger value.
+     * 
+     * Used by push
+     * 
+     * O(log(size()))
+     * 
+     * @param index the current position to swap upwards
+     */
+    void upheap(size_type index) {
+        // TODO
+		//can't move up if at the root
+        if(index == 0){
+            return;
+        }
+
+		//find parent node's index
+        size_t parent_index = parent(index);
+
+		//check if max heap property isn't violated
+        if(comp(c[parent_index], c[index])){
+			//swap to move larger value upward
+            std::swap(c[parent_index], c[index]);
+			//keep bringing value up heap recursively
+            upheap(parent_index);
+        }
+    }
+    
+    /**
+     * @brief Recursively swap the value at index down the heap until it is in the correct position.
+     * 
+     * @note This is a max heap, so assume Compare is less, so promote the larger value.
+     * 
+     * Used by pop
+     * 
+     * O(log(size()))
+     * 
+     * @param index the current position to swap downwards
+     */
+    void downheap(size_type index) {
+        // TODO
+		//if the node is a leaf, t's already in the correct place
+        if(is_leaf(index)){
+            return;
+        }
+
+		//get indices of left and right children
+        size_t left_index = left_child(index);
+        size_t right_index = right_child(index);
+
+		//initially assume that left child is larger
+        size_t max_child_index = left_index;
+
+        if(right_index < c.size()){
+			//check if right child is larger than left child
+            if(comp(c[left_index], c[right_index])){
+                max_child_index = right_index;
+            }
+        }
+
+		//check if current node is smaller than larger child
+        if(comp(c[index], c[max_child_index])){
+			//swap nodes to not violate max heap property
+            std::swap(c[index], c[max_child_index]);
+			//keep pushing value down heap recursively
+            downheap(max_child_index);
+        }
+    }
+
+public:
+    PriorityQueue() = default;
+    PriorityQueue( const PriorityQueue& other ) = default;
+    PriorityQueue( PriorityQueue&& other ) = default;
+    ~PriorityQueue() = default;
+    PriorityQueue& operator=( const PriorityQueue& other ) = default;
+    PriorityQueue& operator=( PriorityQueue&& other ) = default;
+
+    /**
+     * @brief Return a const reference to the element at the top of the heap.
+     * 
+     * O(1)
+     * 
+     * @return const_reference to the element at the top of the heap.
+     */
+    const_reference top() const { /* TODO */ 
+        return c[0];
+    }
+
+    /**
+     * @brief Return whether the heap is empty. This is the same as whether the underlying container, c, is empty.
+     * 
+     * O(1)
+     * 
+     * @return true c is empty
+     * @return false c is not empty
+     */
+    bool empty() const { /* TODO */ 
+        return c.empty();
+    }
+
+    /**
+     * @brief Return the number of elements in the heap. This is the same as the number of elements in the underlying container, c.
+     * 
+     * O(1)
+     * 
+     * @return size_type of the number of elements in the heap
+     */
+    size_type size() const { /* TODO */ 
+        return c.size();
+    }
+	
+    /**
+     * @brief Inserts element and sorts the underlying container, c.
+     * 
+     * Uses upheap
+     * 
+     * O(log(size())) 
+     * 
+     * @param value inserted by copying into c 
+     */
+    void push( const value_type& value ) {
+        // TODO
+        c.push_back(value);
+        upheap(size() - 1);
+    }
+
+    /**
+     * @brief Inserts element and sorts the underlying container, c.
+     * 
+     * Uses upheap
+     * 
+     * O(log(size())) 
+     * 
+     * @param value inserted by moving into c 
+     */
+	void push( value_type&& value ) {
+        // TODO
+        c.push_back(std::move(value));
+        upheap(size() - 1);        
+    }
+
+    /**
+     * @brief Removes the top element.
+     * 
+     * Uses downheap
+     * 
+     * O(log(size())) 
+     * 
+     */
+    void pop() {
+        // TODO
+        std::swap(c[0], c[size() - 1]);
+        c.pop_back();
+        downheap(0);
+    }
+};
